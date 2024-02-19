03c27bd5d534771597747b83a477e4a4ddc8edb5
0x1A0A <mask.monkey1994@gmail.com>
2024-02-19 13:25:52 +0700

    style: adjust order of waybar

--- .config/nvim/lua/core/plugins/lsp.lua
+++ .config/nvim/lua/core/plugins/lsp.lua
@@ -45,7 +45,16 @@ lspconfig["bashls"].setup({
 	on_attach = on_attach,
 })
 
-lspconfig["volar"].setup{
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+--lspconfig["volar"].setup{
+--  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+--}
+
+
+lspconfig.denols.setup {
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 }
 
+lspconfig.tsserver.setup {
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}
