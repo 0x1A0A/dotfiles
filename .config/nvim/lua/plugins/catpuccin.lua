85dd5ff90b52456a4e7d11b262fbd7cdfdeec7a6
0x1A0A <mask.monkey1994@gmail.com>
2024-06-07 08:48:32 +0700

    refactor separate config for completion

--- .config/nvim/lua/plugins/catpuccin.lua
+++ .config/nvim/lua/plugins/catpuccin.lua
@@ -1,7 +1,9 @@
 return {
-	{	"catppuccin/nvim" , name = catpuccin,
-	config = function() 
-		vim.cmd.colorscheme("catppuccin")
-	end,
-	}
+	{
+		"catppuccin/nvim",
+		name = "catpuccin",
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
