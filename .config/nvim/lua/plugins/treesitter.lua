6ae182868f114f66d180a37913f1171e8f001b87
0x1A0A <mask.monkey1994@gmail.com>
2024-06-25 00:05:49 +0700

    chore: add diff to treesitter ensure list

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,18 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "go", "javascript", "rust", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+				ensure_installed = {
+					"go",
+					"javascript",
+					"rust",
+					"typescript",
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"diff",
+				},
 				sync_install = false,
 				auto_install = true,
 				highlight = {
