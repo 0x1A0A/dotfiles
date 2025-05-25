e9d3206650c180e4d004b5ea4be9fd2c3dd132de
0x1A0A <mask.monkey1994@gmail.com>
2025-05-25 22:04:10 +0700

    refactor!: migrate lsp to vim lsp

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,34 +1,32 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-vsnip",
-			"hrsh7th/vim-vsnip",
-			"rafamadriz/friendly-snippets",
-		},
-		config = function()
-			local cmp = require("cmp")
-
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-o>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				snippet = {
-					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
-					end,
-				},
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "vsnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-vsnip",
+		"hrsh7th/vim-vsnip",
+		"rafamadriz/friendly-snippets",
 	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-o>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			snippet = {
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body)
+				end,
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "vsnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,112 +1,91 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			{
-				"williamboman/mason.nvim",
-				build = ":MasonUpdate",
-			},
-			"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"williamboman/mason.nvim",
+			build = ":MasonUpdate",
 		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup()
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup()
 
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.get_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.get_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+		vim.keymap.set("n", "[d", vim.diagnostic.get_prev)
+		vim.keymap.set("n", "]d", vim.diagnostic.get_next)
+		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+		-- Use LspAttach autocommand to only map the following keys
+		-- after the language server attaches to the current buffer
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group =
+				vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				-- Enable completion triggered by <c-x><c-o>
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					--    vim.keymap.set('n', '<space>fm', function()
-					--      vim.lsp.buf.format { async = true }
-					--    end, opts)
-				end,
-			})
+				-- Buffer local mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local opts = { buffer = ev.buf }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+				vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+				vim.keymap.set("n", "<space>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, opts)
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			end,
+		})
 
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "haskell",
-				callback = function()
-					vim.opt.et = true
-					vim.opt.list = true
-				end,
-			})
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "haskell",
+			callback = function()
+				vim.opt.et = true
+				vim.opt.list = true
+			end,
+		})
 
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
 
-			local get_servers = require("mason-lspconfig").get_installed_servers
-			for _, server_name in ipairs(get_servers()) do
-				server_name = server_name == "tsserver" and "ts_ls" or server_name
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end
+		vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
 
-			lspconfig["gdscript"].setup({
-				capabilities = capabilities,
-			})
+		local enable_server = {
+			"gdscript", "bashls", "denols", "ts_ls", "glasgow", "hls", "rust_analyzer"
+		}
 
-			lspconfig["bashls"].setup({
-				capabilities = capabilities,
-			})
+		for _, server in ipairs(enable_server) do
+			vim.lsp.enable(server)
+		end
 
-			lspconfig.denols.setup({
-				root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-			})
+		vim.lsp.config("denols", {
+			root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+		})
 
-			lspconfig["volar"].setup({})
-
-			lspconfig.ts_ls.setup({
-				init_options = {
-					plugins = {
-						{
-							name = "@vue/typescript-plugin",
-							location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-							languages = { "javascript", "typescript", "vue" },
-						},
+		vim.lsp.config("ts_ls", {
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+						languages = { "javascript", "typescript", "vue" },
 					},
 				},
-				root_dir = lspconfig.util.root_pattern("package.json"),
-				single_file_support = false,
-				capabilities = capabilities,
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-			})
+			},
+			root_dir = lspconfig.util.root_pattern("package.json"),
+			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+		})
 
-			lspconfig["glasgow"].setup({})
-
-			lspconfig["hls"].setup({
-				filetypes = { "haskell", "lhaskell", "cabal" },
-			})
-
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
+		vim.lsp.config("hls", {
+			filetypes = { "haskell", "lhaskell", "cabal" },
+		})
+	end,
 }
