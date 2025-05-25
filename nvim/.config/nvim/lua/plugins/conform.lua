8bdd7a1b7b97fd83d75654c6651bd375f2410f95
0x1A0A <mask.monkey1994@gmail.com>
2025-05-25 19:57:57 +0700

    feat: update conform plugins

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,53 +1,54 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		config = function()
-			local conform = require("conform")
+	"stevearc/conform.nvim",
+	lazy = false,
+	config = function()
+		local conform = require("conform")
 
-			local format = {
-				lua = { "stylua" },
-				rust = { "rustfmt" },
-				go = { "gofmt" },
-				sh = { "shfmt" },
-				gdscript = { "gdformat" },
-				cpp = { "clang_format" },
-				c = { "clang_format" },
-				java = { "clang_format" },
-				toml = { "taplo" },
-				haskell = { "fourmolu" },
-			}
 
-			local prettier = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"css",
-				"scss",
-				"html",
-				"yaml",
-				"json",
-				"markdown",
-				"vue",
-				"astro",
-			}
+		local format = {
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+			go = { "gofmt" },
+			sh = { "shfmt" },
+			gdscript = { "gdformat" },
+			cpp = { "clang_format" },
+			c = { "clang_format" },
+			java = { "clang_format" },
+			toml = { "taplo" },
+			haskell = { "fourmolu" },
+		}
 
-			for _, key in ipairs(prettier) do
-				format[key] = { "biome", "prettierd" }
-			end
+		local prettier = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"css",
+			"scss",
+			"html",
+			"yaml",
+			"json",
+			"markdown",
+			"vue",
+			"astro",
+		}
 
-			conform.setup({
-				formatters_by_ft = format,
+		for _, key in ipairs(prettier) do
+			format[key] = { "biome", "prettierd" }
+		end
+
+		conform.setup({
+			formatters_by_ft = format,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+			conform.format({
+				lsp_format = "fallback",
+				timeout_ms = 500,
+				async = false,
 			})
+		end)
 
-			vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-				conform.format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 500,
-				})
-			end)
-		end,
-	},
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
