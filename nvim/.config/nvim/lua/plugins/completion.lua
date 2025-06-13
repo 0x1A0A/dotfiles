e8e8e8c0d7757508352c1decf24593ded8a3197d
miles <mask.monkey1994@gmail.com>
2025-06-13 12:31:42 +0700

    chore: add fallback to tab completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,30 +1,30 @@
 return {
-	'saghen/blink.cmp',
-	dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-	version = '1.*',
+	version = "1.*",
 
 	opts = {
 		keymap = {
-			preset = 'default',
-			['<Tab>'] = { 'select_and_accept' },
-			["<C-o>"] = { 'show', 'show_documentation', 'hide_documentation' },
-			['<S-Tab>'] = { 'snippet_forward', 'fallback' },
+			preset = "default",
+			["<Tab>"] = { "select_and_accept", "fallback" },
+			["<C-o>"] = { "show", "show_documentation", "hide_documentation" },
+			["<S-Tab>"] = { "snippet_forward", "fallback" },
 		},
 
 		appearance = {
-			nerd_font_variant = 'mono'
+			nerd_font_variant = "mono",
 		},
 
 		completion = { documentation = { auto_show = false } },
 
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
 		signature = { enabled = true },
 
-		fuzzy = { implementation = "prefer_rust_with_warning" }
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
