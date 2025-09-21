588a9bbc02ec88ee72c8fbbc59c0c23310766296
0x1A0A <mask.monkey1994@gmail.com>
2025-09-21 16:11:54 +0700

    fix: add epoch time to file when taking screenshot

--- scr/screenshot.sh
+++ scr/screenshot.sh
@@ -3,26 +3,26 @@
 if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
 	case $1 in
 		d)
-			grim -t png - | tee ~/Pictures/Screenshots/$(date +"d-%b-%I-%M").png | wl-copy
+			grim -t png - | tee ~/Pictures/Screenshots/$(date +"d%s-%b-%I-%M").png | wl-copy
 			;;
 		w)
 			window=$(hyprctl activewindow | awk '/at[:]/ {print $2}; /size[:]/ {print $2}' | tr '\n' ' ' | sed 's/\(.*\),\(.*\) /\1x\2/')
-			grim -t png -g "$window" - | tee ~/Pictures/Screenshots/$(date +"w-%b-%I-%M").png | wl-copy
+			grim -t png -g "$window" - | tee ~/Pictures/Screenshots/$(date +"w%s-%b-%I-%M").png | wl-copy
 			;;
 		s)
-			grim -t png -g "$(slurp)" - | tee ~/Pictures/Screenshots/$(date +"s-%b-%I-%M").png | wl-copy
+			grim -t png -g "$(slurp)" - | tee ~/Pictures/Screenshots/$(date +"s%s-%b-%I-%M").png | wl-copy
 			;;
 	esac
 else
 	case $1 in
 		d)
-			maim -i root | tee ~/Pictures/Screenshots/$(date +"d-%b-%I-%M").png | xclip -selection clipboard -t image/png
+			maim -i root | tee ~/Pictures/Screenshots/$(date +"d%s-%b-%I-%M").png | xclip -selection clipboard -t image/png
 			;;
 		w)
-			maim -st 9999998 | tee ~/Pictures/Screenshots/$(date +"w-%b-%I-%M").png | xclip -selection clipboard -t image/png
+			maim -st 9999998 | tee ~/Pictures/Screenshots/$(date +"w%s-%b-%I-%M").png | xclip -selection clipboard -t image/png
 			;;
 		s)
-			maim -s | tee ~/Pictures/Screenshots/$(date +"s-%b-%I-%M").png | xclip -selection clipboard -t image/png
+			maim -s | tee ~/Pictures/Screenshots/$(date +"s%s-%b-%I-%M").png | xclip -selection clipboard -t image/png
 			;;
 	esac
 fi
