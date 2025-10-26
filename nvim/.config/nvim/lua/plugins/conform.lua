aaf6f46ba047e6e5a57138f7e4a415aacbce7757
0x1A0A <mask.monkey1994@gmail.com>
2025-10-26 22:03:47 +0700

    refactor(nvim): rewrite conform to use lazy loading

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,53 +1,60 @@
+local format = {
+	lua = { "stylua" },
+	rust = { "rustfmt" },
+	go = { "gofmt" },
+	sh = { "shfmt" },
+	gdscript = { "gdformat" },
+	cpp = { "clang_format" },
+	c = { "clang_format" },
+	java = { "clang_format" },
+	toml = { "taplo" },
+	haskell = { "fourmolu" },
+}
+
+local prettier = {
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"css",
+	"scss",
+	"html",
+	"yaml",
+	"json",
+	"markdown",
+	"vue",
+	"astro",
+}
+
+for _, key in ipairs(prettier) do
+	format[key] = { "biome", "prettierd" }
+end
+
+---@type LazySpec
 return {
 	"stevearc/conform.nvim",
-	lazy = false,
-	config = function()
-		local conform = require("conform")
-
-		local format = {
-			lua = { "stylua" },
-			rust = { "rustfmt" },
-			go = { "gofmt" },
-			sh = { "shfmt" },
-			gdscript = { "gdformat" },
-			cpp = { "clang_format" },
-			c = { "clang_format" },
-			java = { "clang_format" },
-			toml = { "taplo" },
-			haskell = { "fourmolu" },
-		}
-
-		local prettier = {
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"css",
-			"scss",
-			"html",
-			"yaml",
-			"json",
-			"markdown",
-			"vue",
-			"astro",
-		}
-
-		for _, key in ipairs(prettier) do
-			format[key] = { "biome", "prettierd" }
-		end
-
-		conform.setup({
-			formatters_by_ft = format,
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-			conform.format({
-				lsp_format = "fallback",
-				timeout_ms = 500,
-				async = false,
-			})
-		end)
-
+	event = { "BufWritePre" },
+	lazy = true,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				require("conform").format({ async = false })
+			end,
+			mode = "",
+			desc = "Format buffer using conform",
+		},
+	},
+	---@type conform.setupOpts
+	opts = {
+		formatters_by_ft = format,
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 }
