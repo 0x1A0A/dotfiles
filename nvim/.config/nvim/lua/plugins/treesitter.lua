609a8869bd0b71d3a6427194e6f35c750e3d2ed2
0x1A0A <mask.monkey1994@gmail.com>
2025-10-26 22:37:05 +0700

    refactor(nvim): move opts to LazySpec for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,29 @@
 ---@diagnostic disable: missing-fields, undefined-field
+---@type LazySpec
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"go",
-				"javascript",
-				"rust",
-				"typescript",
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"diff",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
+	opts = {
+		ensure_installed = {
+			"go",
+			"javascript",
+			"rust",
+			"typescript",
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"diff",
+		},
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
