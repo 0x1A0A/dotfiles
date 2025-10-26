04f519281cd3851fd32c41550b09fc99d2e3b5b7
0x1A0A <mask.monkey1994@gmail.com>
2025-10-26 22:37:39 +0700

    refactor(nvim): telescope move option to and keymap to LazySpec

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,42 +1,41 @@
+---@type LazySpec
 return {
-
 	"nvim-telescope/telescope.nvim",
-	lazy = false,
+	lazy = true,
 	branch = "0.1.x",
+	cmd = "Telescope",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
-	config = function()
-		local builtin = require("telescope.builtin")
-
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fv", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", mode = "n" },
+		{ "<leader>fv", "<cmd>Telescope git_files<cr>", mode = "n" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", mode = "n" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", mode = "n" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", mode = "n" },
+	},
+	opts = {
+		extensions = {
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+		},
+	},
+	config = function(_, opts)
 		local actions = require("telescope.actions")
 
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<m-d>"] = actions.delete_buffer + actions.move_to_top,
-					},
-				},
-			},
-			extensions = {
-				fzf = {
-					fuzzy = true, -- false will only do exact matching
-					override_generic_sorter = true, -- override the generic sorter
-					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-					-- the default case_mode is "smart_case"
-				},
-			},
-		})
+		local mappings = {
+			i = { ["<m-d>"] = actions.delete_buffer + actions.move_to_top },
+		}
 
+		opts.defaults = { mappings = mappings }
+
+		require("telescope").setup(opts)
 		require("telescope").load_extension("fzf")
 	end,
 }
