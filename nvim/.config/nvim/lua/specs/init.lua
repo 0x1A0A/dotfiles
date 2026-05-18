5b0e89220c8d10d95f3a6b2c9f138fb219f74c21
0x1A0A <mask.monkey1994@gmail.com>
2026-05-18 21:07:28 +0700

    refactor(nvim): move fzf lua to misc

--- nvim/.config/nvim/lua/specs/init.lua
+++ nvim/.config/nvim/lua/specs/init.lua
@@ -4,6 +4,5 @@ return {
 	{ import = "specs.lsp" },
 	{ import = "specs.completion" },
 	{ import = "specs.formatter" },
-	{ import = "specs.picker" },
 	{ import = "specs.dap" },
 }
--- nvim/.config/nvim/lua/specs/misc.lua
+++ nvim/.config/nvim/lua/specs/misc.lua
@@ -64,4 +64,20 @@ return {
 			{ "<leader>vc", vim.cmd.Git },
 		},
 	},
+	{
+		"fzf-lua",
+		lazy = false,
+		cmd = "FzfLua",
+		keys = {
+			{ "<leader>fz", vim.cmd.FzfLua, mode = "n" },
+			{ "<leader>ff", "<cmd>FzfLua files<CR>", mode = "n" },
+			{ "<leader>fv", "<cmd>FzfLua git_files<CR>", mode = "n" },
+			{ "<leader>fg", "<cmd>FzfLua live_grep<CR>", mode = "n" },
+			{ "<leader>fb", "<cmd>FzfLua buffers<CR>", mode = "n" },
+			{ "<leader>fh", "<cmd>FzfLua helptags<CR>", mode = "n" },
+		},
+		after = function()
+			require("fzf-lua").setup({ ui_select = true })
+		end,
+	},
 }
